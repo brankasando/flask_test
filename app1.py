7eb805905cc8bfbb2dfc59d1c8843b07cc458327
branka <branka.sando@hotmail.com>
2022-05-17 12:07:41 +0200

    Prikazi drop-down i tabelu

--- app1.py
+++ app1.py
@@ -16,15 +16,45 @@ def print_hello():
 
 @app.route('/show_db', methods=['GET','POST'])
 def get_dropdown_():
-    con = sqlite3.connect('/home/branka/test1/bets.db')
-    cur = con.cursor()
-    cur.execute(
-        '''
-        select distinct col1 from test_table
-        ''')
-    drop_down_list = cur.fetchall()
-    return render_template('show_db.html', ddl=drop_down_list)
+    if request.method == 'GET':
+        con = sqlite3.connect('/home/branka/test1/bets.db')
+        cur = con.cursor()
+        cur.execute(
+            '''
+            select 'All' as col1
+            union
+            select distinct col1 from test_table 
+            ''')
+        drop_down_list_fetched = cur.fetchall()
 
+        col1_value = request.args.get('col1')
+
+        if col1_value is None:
+            col1_value = 'All'
+
+        cur.execute(
+            '''
+            select * from test_table where col1 = (?) or 'All' = (?)
+            ''',
+            (col1_value, col1_value) )
+
+        rows_fetched= cur.fetchall()
+        return render_template('show_db.html', ddl=drop_down_list_fetched, rows=rows_fetched, c=col1_value)
+    else:
+        con = sqlite3.connect('/home/branka/test1/bets.db')
+        cur = con.cursor()
+        cur.execute(
+            '''
+            select distinct col1 from test_table 
+            ''')
+        drop_down_list_fetched = cur.fetchall()
+
+        cur.execute(
+            '''
+            select * from test_table
+            ''')
+        rows_fetched= cur.fetchall()
+    return render_template('show_db.html', ddl=drop_down_list_fetched, rows=rows_fetched)
 
 if __name__ == "__main__":
     app.run(debug=True) #ako zovemo program iz komande linije da ukljuci debug mode
